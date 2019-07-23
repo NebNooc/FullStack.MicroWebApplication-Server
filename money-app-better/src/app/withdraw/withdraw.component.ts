b777c8033f845bb7dfd19c549ea71ef9f9be77d9
robertgordon <robertgordon.subscript2@gmail.com>
2019-07-23 15:33:41 -0400

    tweeked withdraw in TransactionController

--- money-app-better/src/app/withdraw/withdraw.component.ts
+++ money-app-better/src/app/withdraw/withdraw.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../model/user';
+import { Account } from '../../model/account';
+import { UserService } from 'src/service/user.service';
+import { AccountServiceService } from 'src/service/account-service.service';
+import { TransactionService } from 'src/service/transaction.service';
 
 @Component({
   selector: 'app-withdraw',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WithdrawComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+  selectedAccount: Account;
+  userAccounts: Account[];
+  withdrawAmount: number;
+
+  constructor(private userService: UserService,
+    private accountService: AccountServiceService,
+    private transactionService: TransactionService) { }
 
   ngOnInit() {
+    this.currentUser = this.userService.getUser();
+    this.getUserAccounts();
+  }
+
+  getUserAccounts(){
+    this.accountService.getAccountsByUser(this.currentUser.id).subscribe(accounts => this.userAccounts = accounts);
+  }
+
+  withdraw(){
+    this.transactionService.withdraw(this.currentUser.id, this.withdrawAmount).subscribe(() => this.getUserAccounts())
   }
 
 }
