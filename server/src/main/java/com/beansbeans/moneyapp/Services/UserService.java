07c060e6fc884f9f28d388ef27902d6696d7031b
Henry <cataniahf@gmail.com>
2019-07-31 14:29:24 -0400

    some tests

--- server/src/main/java/com/beansbeans/moneyapp/Services/UserService.java
+++ server/src/main/java/com/beansbeans/moneyapp/Services/UserService.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.List;
+
 
 @Service
 public class UserService {
--- server/src/test/java/com/beansbeans/moneyapp/ControllerTests/TransactionControllerTest.java
+++ server/src/test/java/com/beansbeans/moneyapp/ControllerTests/TransactionControllerTest.java
@@ -1,4 +1,91 @@
 package com.beansbeans.moneyapp.ControllerTests;
 
+import com.beansbeans.moneyapp.Controllers.TransactionController;
+import com.beansbeans.moneyapp.Model.Transaction;
+import com.beansbeans.moneyapp.Services.TransactionService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
 public class TransactionControllerTest {
+
+    Transaction testTransaction = new Transaction(4L,5L,6L,300.0,"this is a memo", LocalDateTime.now(), 1L);
+
+    @Mock
+    private TransactionService transactionService;
+
+    @InjectMocks
+    private TransactionController transactionController;
+
+
+    @Test
+    public void createTransactionTest(){
+        Transaction transaction = mockTransaction();
+
+        when(transactionService.newTransaction(transaction)).thenReturn(transaction);
+        transactionController.createTransaction(transaction);
+
+        verify(transactionService, times(1)).newTransaction(transaction);
+    }
+
+    @Test
+    public void findByIdTest(){
+        when(transactionService.findTransactionById(1L)).thenReturn(testList().get(1));
+        transactionController.findById(1L);
+
+        verify(transactionService, times(1)).findTransactionById(1L);
+    }
+
+    @Test
+    public void findByUserIdTest(){
+        when(transactionService.findTransactionByUserId(9L)).thenReturn(testList().get(1));
+        transactionController.findByUserId(9L);
+
+        verify(transactionService, times(1)).findTransactionByUserId(9L);
+    }
+
+    @Test
+    public void getAllTransactionsTest(){
+        when(transactionService.getAllTransactions()).thenReturn(testList());
+        transactionController.getAllTransactions();
+
+        verify(transactionService, times(1)).getAllTransactions();
+    }
+
+    @Test
+    public void depositTest(){
+        when(transactionService.depositTo(6L, 300.0)).thenReturn(true);
+        transactionController.deposit(testTransaction);
+
+        verify(transactionService, times(1 )).depositTo(6L,300.0);
+    }
+
+    @Test
+    public void withdrawTest(){
+        when(transactionService.withdrawFrom(6L, 300.0)).thenReturn(true);
+        transactionController.withdraw(testTransaction);
+    }
+
+    private Transaction mockTransaction(){
+        return new Transaction(1L, 2L,3L,100.0, "this is a memo", LocalDateTime.now(), 7L);
+    }
+
+    private List<Transaction> testList(){
+        Transaction transaction1 = new Transaction(2L,3L,4L,200.0, "this is a memo", LocalDateTime.now(), 8L);
+        Transaction transaction2 = new Transaction(3L,4L,5L,400.0, "this is a memo", LocalDateTime.now(),9L);
+
+        return Arrays.asList(transaction1, transaction2);
+    }
 }
+
