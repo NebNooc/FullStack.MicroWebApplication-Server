4ca5bb264d92e91bf36669af7caaf59caeef97ed
robertgordon <robertgordon.subscript2@gmail.com>
2019-08-01 10:03:19 -0400

    tweaking transactionservice

--- server/src/main/java/com/beansbeans/moneyapp/Services/TransactionService.java
+++ server/src/main/java/com/beansbeans/moneyapp/Services/TransactionService.java
@@ -61,12 +61,13 @@ public class TransactionService {
     public Boolean transferFunds(Long fromId, Long toId, Double amount){
         Account fromAccount = accountRepository.findById(fromId).get();
         Account toAccount = accountRepository.findById(toId).get();
-        Double initalBalance = fromAccount.getBalance();
-        if((initalBalance - amount) < 0.00){
+        Double fromInitialBalance = fromAccount.getBalance();
+        Double toInitialBalance = toAccount.getBalance();
+        if((fromInitialBalance - amount) < 0.00){
             throw new IllegalArgumentException("Unable to complete Transaction.  Insufficient Funds");
         }
-        fromAccount.setBalance(initalBalance - amount);
-        toAccount.setBalance(initalBalance + amount);
+        fromAccount.setBalance(fromInitialBalance - amount);
+        toAccount.setBalance(toInitialBalance + amount);
         accountRepository.save(fromAccount);
         accountRepository.save(toAccount);
         return true;
